d8a9b9b0ae6081b4c58723ade94452c3570b9134
Simon-Oliver <stauffer.simon@outlook.com>
2019-07-24 22:05:10 +1000

    installed express

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
-var simon = 'something';
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app
+  .use(express.static(path.join(__dirname, '/client/build')))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+app.get('/', (req, res) => {
+  console.log('working');
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
